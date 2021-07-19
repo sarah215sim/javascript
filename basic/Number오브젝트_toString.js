3015e0c792f7ec1fbeb8065f6cdc765a6c1b91d6
sarah215sim <sarah215sim@gmail.com>
2021-07-19 15:31:38 +0900

    toExponential & toFixed

--- basic/Number오브젝트_toString.js
+++ basic/Number오브젝트_toString.js
@@ -13,6 +13,6 @@ console.log(typeof valueToStiring); // 실행결과 string
 
 /* 변수가 아닌 숫자자체에 toString을 사용하는 경우, 유동소수점 사용 주의 사항 */
 
-console.log(20.toString()); // 오류발생 .을 유동소수점(즉, 20.0으로 인식하므로)
+// console.log(20.toString()); // 오류발생 .을 유동소수점(즉, 20.0으로 인식하므로)
 console.log(20..toString()); // 첫번째 .은 유동소수점 / 두번째 .은 값과 함수를 구분하는 문법(.toString())
 // IEEE754 규정은 준수하기 때문에 발생 = JS는 실수로 처리하기 때문(20이 정수가 아닌 실수라는 뜻)
