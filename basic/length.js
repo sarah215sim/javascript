591db6e724f6f399f8a0f3f4cd0d2e94e5dbae21
Sarah Sim <78900449+sarah215sim@users.noreply.github.com>
2021-07-22 00:13:17 +0900

    Update length.js

--- basic/length.js
+++ basic/length.js
@@ -21,6 +21,6 @@ console.log(value.length); // 어떻게 length가 3을 반환하는가?
 
 // JS엔진이!!!
 var obj = new String("ABC"); // 자동으로 value의 값 타입을 해석하고 타입에 맞는 인스턴스를 생성
-// 가상의 obj라는 인스턴스 안에 0:"A", 0:"B", 0:"C", length: 3, [[PrimitiveValue]]:"ABC" 와 같은 오브젝트를 생성
+// 가상의 obj라는 인스턴스 안에 0:"A", 0:"B", 0:"C", length: 3, [[PrimitiveValue]]:"ABC" 와 같은 값(프로퍼티)을 가진 오브젝트를 생성
 // 위 값을 value.length 같은 형태로 가져올 수 있다. 여기서 value는 JS가 만든 인스턴스!
 
